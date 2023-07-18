cdf28c5afbdef95afcb191f4042dddb97d7ea0cd
wizard <gg4148092@gmail.com>
2023-07-18 11:46:41 +0300

    fixing bugs

--- calculator.go
+++ calculator.go
@@ -1,83 +1,79 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"os"
-	"strings"
 	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 var roman, arabic bool
 
-func recognizenum(in string) int {
+func recognizenum(in string) (int, error) {
 	out, err := strconv.Atoi(in)
 	if err != nil {
 		if arabic == true {
-			fmt.Println("YOU CAN USE ARABIC OR ROMAN NOT BOTH")
-			os.Exit(1)
+			return 0, errors.New("YOU CAN USE ARABIC OR ROMAN NOT BOTH")
 		}
 		roman = true
 		switch in {
 		case "I":
-			return 1
+			return 1, nil
 		case "II":
-			return 2
+			return 2, nil
 		case "III":
-			return 3
+			return 3, nil
 		case "IV":
-			return 4
+			return 4, nil
 		case "V":
-			return 5
+			return 5, nil
 		case "VI":
-			return 6
+			return 6, nil
 		case "VII":
-			return 7
+			return 7, nil
 		case "VIII":
-			return 8
+			return 8, nil
 		case "IX":
-			return 9
+			return 9, nil
 		case "X":
-			return 10
+			return 10, nil
 		default:
-			fmt.Println("UNKNOWN NUMBER")
-			os.Exit(1)
-        		// ... handle error
-        		//panic(err)
+			return 0, errors.New("UNKNOWN NUMBER")
 		}
 	}
 	if out > 10 || out < 1 {
-		fmt.Println("input numbers from 1 to 10 only handled")
-		os.Exit(1)
+		return 0, errors.New("input numbers from 1 to 10 only handled")
 	}
 	if roman == true {
-		fmt.Println("YOU CAN USE ARABIC OR ROMAN NOT BOTH")
-		os.Exit(1)
+		return 0, errors.New("YOU CAN USE ARABIC OR ROMAN NOT BOTH")
 	}
 	arabic = true
-	return out
+	return out, nil
 }
 
-func arabictoroman(arabicnumeral int) string {
-	arabicmap := map[int]string{
-		500:  "D",
-		100:  "C",
-		50:   "L",
-		10:   "X",
-		5:    "V",
-		1:    "I",
+func arabictoroman(num int) string {
+	if num < 1 || num > 3999 {
+		return "Invalid number"
 	}
+
+	arabic := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	roman := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+
 	result := ""
-	for num, symbol := range arabicmap {
-		for arabicnumeral >= num {
-			result += symbol
-			arabicnumeral -= num
+
+	for i := 0; i < len(arabic); i++ {
+		for num >= arabic[i] {
+			result += roman[i]
+			num -= arabic[i]
 		}
 	}
+
 	return result
 }
 
-func calculate(operand string, a, b int) string {
+func calculate(operand string, a, b int) (string, error) {
 	var result int
 	switch operand {
 	case "+":
@@ -87,20 +83,21 @@ func calculate(operand string, a, b int) string {
 	case "*":
 		result = a * b
 	case "/":
+		if b == 0 {
+			return "", errors.New("cannot divide by zero")
+		}
 		result = a / b
 	default:
-		fmt.Println("UNKNOWN OPERATION")
-		os.Exit(1)
+		return "", errors.New("UNKNOWN OPERATION")
 	}
 	if roman == true && result < 1 {
-		fmt.Println("ROMAN NOTATRION NOT SUPPORT NEGATIVE NUMBERS")
-		os.Exit(1)
+		return "", errors.New("ROMAN NOTATRION NOT SUPPORT NEGATIVE NUMBERS")
 	}
 	if roman == true {
-		return arabictoroman(result)	
+		return arabictoroman(result), nil
 	}
-	return strconv.Itoa(result)
-	
+	return strconv.Itoa(result), nil
+
 }
 
 func main() {
@@ -118,10 +115,28 @@ func main() {
 		fmt.Println("INVALID OPERATION FORMAT")
 		os.Exit(1)
 	}
-	a:= arr[0]
-	b:= arr[2]
-	sign:= arr[1]
+	a := arr[0]
+	b := arr[2]
+	sign := arr[1]
 	//fmt.Println(a, b, sign)
 
-	fmt.Println(calculate(sign, recognizenum(a), recognizenum(b)))
+	aNum, err := recognizenum(a)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	bNum, err := recognizenum(b)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	result, err := calculate(sign, aNum, bNum)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(result)
 }
